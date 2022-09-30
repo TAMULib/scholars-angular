92310fa07fe5b12addd49ca43edc1dd049229439
William Welling <william_welling@harvard.edu>
2022-09-30 11:02:38 -0500

    upgrade dependencies and config

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -53,10 +53,7 @@ import 'zone.js'; // Included with Angular CLI.
 /***************************************************************************************************
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
-import '@angular/localize/init';
-
-
-import 'ie11-custom-properties';
+ import '@angular/localize/init';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
