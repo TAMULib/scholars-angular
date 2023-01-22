7aacce8688c1b4cdd14a50869d67c744836c3617
William Welling <wwelling@library.tamu.edu>
2023-01-22 13:10:43 -0600

    order investigator data fields

--- src/app/+visualization/co-investigator-network/co-investigator-network.component.ts
+++ src/app/+visualization/co-investigator-network/co-investigator-network.component.ts
@@ -44,7 +44,7 @@ export class CoInvestigatorNetworkComponent implements OnDestroy, OnInit {
         this.store.dispatch(new fromSdr.GetNetworkAction('individual', {
           id: params.id,
           dateField: 'dateTimeIntervalStart',
-          dataFields: ['contributors', 'principalInvestigators', 'coPrincipalInvestigators'],
+          dataFields: ['principalInvestigators', 'coPrincipalInvestigators', 'contributors'],
           typeFilter: 'class:Relationship AND type:Grant'
         }));
       }
