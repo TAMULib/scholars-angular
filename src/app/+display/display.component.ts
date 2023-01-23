fee54e53a7972591f53c95b891c72d4f7afd8a66
William Welling <wwelling@library.tamu.edu>
2023-01-23 12:35:15 -0600

    fix navigation between display and visualization

--- src/app/+display/display.component.ts
+++ src/app/+display/display.component.ts
@@ -1,29 +1,22 @@
-import { Component, OnDestroy, OnInit, ChangeDetectionStrategy, PLATFORM_ID, Inject } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
 import { MetaDefinition } from '@angular/platform-browser';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { BehaviorSubject, firstValueFrom, Observable, Subscription } from 'rxjs';
+import { filter, switchMap, take, tap } from 'rxjs/operators';
 
-import { Store, select } from '@ngrx/store';
-
-import { Observable, Subscription, BehaviorSubject } from 'rxjs';
-import { filter, take, switchMap, tap } from 'rxjs/operators';
-
+import { SolrDocument } from '../core/model/discovery';
+import { DiscoveryView, DisplayTabSectionView, DisplayTabView, DisplayView, Filter } from '../core/model/view';
+import { DisplaySubsectionView, Side } from '../core/model/view/display-view';
 import { AppState } from '../core/store';
-
-import { DiscoveryView, DisplayView, DisplayTabView, DisplayTabSectionView, Filter } from '../core/model/view';
-
+import { selectWindowDimensions } from '../core/store/layout';
 import { WindowDimensions } from '../core/store/layout/layout.reducer';
-
+import { selectDiscoveryViewByClass, selectDisplayViewByTypes, selectResourceById, selectResourceIsDereferencing } from '../core/store/sdr';
 import { fadeIn } from '../shared/utilities/animation.utility';
 import { loadBadges } from '../shared/utilities/view.utility';
 
-import { selectWindowDimensions } from '../core/store/layout';
-import { SolrDocument } from '../core/model/discovery';
-import { Side, DisplaySubsectionView } from '../core/model/view/display-view';
-
-import { selectResourceById, selectDiscoveryViewByClass, selectDisplayViewByTypes, selectResourceIsDereferencing } from '../core/store/sdr';
-
-import * as fromSdr from '../core/store/sdr/sdr.actions';
 import * as fromMetadata from '../core/store/metadata/metadata.actions';
+import * as fromSdr from '../core/store/sdr/sdr.actions';
 
 const hasDataAfterFilter = (fltr: Filter, obj: any): boolean => {
   return Array.isArray(obj[fltr.field]) ? obj[fltr.field].indexOf(fltr.value) >= 0 : obj[fltr.field] === fltr.value;
@@ -183,7 +176,7 @@ export class DisplayComponent implements OnDestroy, OnInit {
           );
 
           // subscribe to first defined document to find display view
-          this.subscriptions.push(this.store.pipe(
+          firstValueFrom(this.store.pipe(
             select(selectResourceById('individual', params.id)),
             filter((document: SolrDocument) => document !== undefined),
             take(1),
@@ -253,7 +246,7 @@ export class DisplayComponent implements OnDestroy, OnInit {
                 })
               );
             })
-          ).subscribe());
+          ));
         }
       })
     );
