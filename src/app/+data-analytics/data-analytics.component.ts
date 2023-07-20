bf5f24c592e12774e37866e653a5bbff4cb91bb2
William Welling <wwelling@library.tamu.edu>
2023-07-20 14:00:59 -0500

    Fix lint error

--- src/app/+data-analytics/data-analytics.component.ts
+++ src/app/+data-analytics/data-analytics.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-data-analytics',
+  selector: 'scholars-data-analytics',
   templateUrl: './data-analytics.component.html',
   styleUrls: ['./data-analytics.component.scss']
 })
