95f6b5948c50e659a050d95706ae5a1612f61367
William Welling <wwelling@library.tamu.edu>
2023-07-21 17:33:51 -0500

    More cleanup

--- src/app/data-analytics/data-analytics.component.ts
+++ src/app/data-analytics/data-analytics.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, map } from 'rxjs';
-
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'scholars-data-analytics',
   templateUrl: './data-analytics.component.html',
   styleUrls: ['./data-analytics.component.scss']
 })
-export class DataAnalyticsComponent implements OnInit {
-
-  public view: Observable<string>;
-
-  constructor(
-    private route: ActivatedRoute
-  ) {
-
-  }
-
-
-  ngOnInit(): void {
-    this.view = this.route.params.pipe(map((params) => params.view));
-  }
+export class DataAnalyticsComponent {
 
 }
