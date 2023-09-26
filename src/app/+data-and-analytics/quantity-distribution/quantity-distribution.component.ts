9344323b0a5408c66109a58d8c5a6a4ed3f9d70a
William Welling <144841721+wwhoop@users.noreply.github.com>
2023-09-26 15:30:55 -0500

    Afford multiselect with facet entries modal and apply filters (#296)
    * Facet entry model support multiselect, add/remove sidebar items and apply filters selected
    
    * Apply filters for un sdg visualization
    
    * Remove filter from query params
    
    * Adjust academic age group visualization columns
    
    * Minor cleanup

--- src/app/+data-and-analytics/quantity-distribution/quantity-distribution.component.ts
+++ src/app/+data-and-analytics/quantity-distribution/quantity-distribution.component.ts
@@ -6,7 +6,7 @@ import { Subscription, filter } from 'rxjs';
 import * as d3 from 'd3';
 
 import { SolrDocument } from '../../core/model/discovery';
-import { SidebarMenu } from '../../core/model/sidebar';
+import { SidebarItemType, SidebarMenu } from '../../core/model/sidebar';
 import { DataAndAnalyticsView, DisplayView, Facet, Filter, OpKey } from '../../core/model/view';
 import { DialogService } from '../../core/service/dialog.service';
 import { AppState } from '../../core/store';
@@ -16,6 +16,7 @@ import { fadeIn } from '../../shared/utilities/animation.utility';
 import { id } from '../../shared/utilities/id.utility';
 import { getUNSDGByValue, getUNSDGIndexByValue } from '../../shared/utilities/un-sdg.utility';
 
+import * as fromRouter from '../../core/store/router/router.actions';
 import * as fromSdr from '../../core/store/sdr/sdr.actions';
 import * as fromSidebar from '../../core/store/sidebar/sidebar.actions';
 
@@ -53,6 +54,8 @@ export class QuantityDistributionComponent implements OnChanges, OnDestroy, OnIn
 
   public id: string;
 
+  private sidebarMenuSections: { [key: string]: { facet:Facet, index: number } };
+
   private subscriptions: Subscription[];
 
   constructor(
@@ -62,6 +65,7 @@ export class QuantityDistributionComponent implements OnChanges, OnDestroy, OnIn
   ) {
     this.labelEvent = new EventEmitter<string>();
     this.id = id();
+    this.sidebarMenuSections = {};
     this.subscriptions = [];
   }
 
@@ -72,9 +76,9 @@ export class QuantityDistributionComponent implements OnChanges, OnDestroy, OnIn
   }
 
   ngOnInit(): void {
-    const items = [];
     const menu: SidebarMenu = {
-      sections: this.dataAndAnalyticsView.facets.map((facet: Facet) => {
+      sections: this.dataAndAnalyticsView.facets.map((facet: Facet, index: number) => {
+        this.sidebarMenuSections[facet.field] = { facet, index};
         return {
           title: facet.name,
           expandable: facet.expandable,
@@ -82,7 +86,7 @@ export class QuantityDistributionComponent implements OnChanges, OnDestroy, OnIn
           collapsed: facet.collapsed,
           useDialog: facet.useDialog,
           action: this.dialog.facetEntriesDialog(facet.name, facet.field),
-          items,
+          items: [],
         };
       })
     };
@@ -189,7 +193,40 @@ export class QuantityDistributionComponent implements OnChanges, OnDestroy, OnIn
   ngOnChanges(changes: SimpleChanges): void {
     const { filters } = changes;
 
-    const additionalFilters = [];
+    if (!!filters.previousValue) {
+      filters.previousValue.forEach((entry: any) => {
+        if (filters.currentValue.indexOf(entry) === -1) {
+          const section = this.sidebarMenuSections[entry.field];
+          if (!!section) {
+            this.store.dispatch(new fromSidebar.RemoveSectionAction({
+              sectionIndex: section.index,
+              itemLabel: entry.value,
+              itemField: entry.field,
+            }));
+          }
+        }
+      });
+    }
+
+    filters.currentValue.forEach((entry: any) => {
+      if (filters.previousValue === undefined || filters.previousValue.indexOf(entry) === -1) {
+        const section = this.sidebarMenuSections[entry.field];
+        if (!!section) {
+          this.store.dispatch(new fromSidebar.AddSectionItemAction({
+            sectionIndex: section.index,
+            sectionItem: {
+              type: SidebarItemType.ACTION,
+              label: entry.value,
+              selected: true,
+              action: new fromRouter.RemoveFilter({ filter: entry }),
+            }
+          }));
+        }
+      }
+    });
+
+    const additionalFilters = [...filters.currentValue];
+    additionalFilters.shift();
 
     if (this.organization.id !== this.defaultId && !!this.organization.name) {
       additionalFilters.push({
--- src/app/core/store/router/router.actions.ts
+++ src/app/core/store/router/router.actions.ts
@@ -1,5 +1,6 @@
 import { Action } from '@ngrx/store';
 import { NavigationExtras } from '@angular/router';
+import { Filter } from '../../model/view';
 
 export type RouterNavigation = Readonly<{
   path: any[];
@@ -13,6 +14,7 @@ export enum RouterActionTypes {
   BACK = '[Router] back',
   FORWARD = '[Router] forward',
   CHANGED = '[Router] changed',
+  REMOVE_FILTER = '[Router] remove filter',
 }
 
 export class Go implements Action {
@@ -37,9 +39,15 @@ export class Changed implements Action {
   readonly type = RouterActionTypes.CHANGED;
 }
 
+export class RemoveFilter implements Action {
+  readonly type = RouterActionTypes.REMOVE_FILTER;
+  constructor(public payload: { filter: Filter }) { }
+}
+
 export type RouterActions =
   Go |
   Link |
   Back |
   Forward |
-  Changed;
+  Changed |
+  RemoveFilter;
