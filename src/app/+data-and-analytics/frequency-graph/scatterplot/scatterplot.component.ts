9c9c7558fc5fa422be8630b6c556423d4e2cd40c
Dbreck-TAMU <dbreckenridge@tamu.edu>
2025-02-11 08:35:30 -0600

    Code refactor

--- src/app/+data-and-analytics/frequency-graph/scatterplot/scatterplot.component.ts
+++ src/app/+data-and-analytics/frequency-graph/scatterplot/scatterplot.component.ts
@@ -32,9 +32,7 @@ export class ScatterplotComponent implements OnInit, OnChanges, AfterViewInit, O
   private svg: d3.Selection<SVGGElement, unknown, HTMLElement, any>;
 
   private margin = { top: 20, right: 20, bottom: 30, left: 50 };
-
   private width: number;
-
   private height: number;
 
   private dataSubscription: Subscription;
@@ -42,7 +40,6 @@ export class ScatterplotComponent implements OnInit, OnChanges, AfterViewInit, O
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit(): void {
-    
   }
 
   ngAfterViewInit(): void {
@@ -90,9 +87,95 @@ export class ScatterplotComponent implements OnInit, OnChanges, AfterViewInit, O
     }
   }
 
+  private createScales(
+    startDate: Date,
+    endDate: Date,
+    maxCount: number
+  ): { xScale: d3.ScaleTime<number, number>; yScale: d3.ScaleLinear<number, number> } {
+    const xScale = d3.scaleTime()
+      .domain([startDate, endDate])
+      .range([0, this.width]);
+
+    const yScale = d3.scaleLinear()
+      .domain([0, maxCount])
+      .nice()
+      .range([this.height, 0]);
+
+    return { xScale, yScale };
+  }
+
+  private drawGridAndAxes(
+    xScale: d3.ScaleTime<number, number>,
+    yScale: d3.ScaleLinear<number, number>,
+    gridTickInterval: number,
+    tickValues?: Date[]
+  ): void {
+    const xAxisGenerator = tickValues
+      ? d3.axisBottom(xScale).tickValues(tickValues).tickFormat(d3.timeFormat('%Y'))
+      : d3.axisBottom(xScale).ticks(d3.timeYear.every(gridTickInterval)).tickFormat(d3.timeFormat('%Y'));
+
+    const xGridGenerator = tickValues
+      ? d3.axisBottom(xScale).tickValues(tickValues).tickSize(-this.height).tickFormat(() => '')
+      : d3.axisBottom(xScale).ticks(d3.timeYear.every(gridTickInterval)).tickSize(-this.height).tickFormat(() => '');
+
+    const xGrid = this.svg.append('g')
+      .attr('class', 'grid x-grid')
+      .attr('transform', `translate(0, ${this.height})`)
+      .call(xGridGenerator);
+    xGrid.select('.domain').remove();
+    xGrid.selectAll('line')
+      .attr('stroke', 'gray')
+      .attr('stroke-opacity', 0.7);
+
+    const yGrid = this.svg.append('g')
+      .attr('class', 'grid y-grid')
+      .call(d3.axisLeft(yScale).ticks(5).tickSize(-this.width).tickFormat(() => ''));
+    yGrid.select('.domain').remove();
+    yGrid.selectAll('line')
+      .attr('stroke', 'gray')
+      .attr('stroke-opacity', 0.7);
+
+    this.svg.append('g')
+      .attr('transform', `translate(0, ${this.height})`)
+      .call(xAxisGenerator);
+
+    this.svg.append('g')
+      .call(d3.axisLeft(yScale).ticks(5));
+  }
+
+  private drawRightBorder(): void {
+    this.svg.append('line')
+      .attr('class', 'right-border')
+      .attr('x1', this.width)
+      .attr('y1', 0)
+      .attr('x2', this.width)
+      .attr('y2', this.height)
+      .attr('stroke', 'black')
+      .attr('stroke-width', 1);
+  }
+
+  private drawLine(
+    seriesData: Array<{ date: Date; count: number }>,
+    color: string,
+    xScale: d3.ScaleTime<number, number>,
+    yScale: d3.ScaleLinear<number, number>
+  ): void {
+    const lineGenerator = d3.line<{ date: Date; count: number }>()
+      .x(d => xScale(d.date))
+      .y(d => yScale(d.count))
+      .curve(d3.curveLinear);
+
+    this.svg.append('path')
+      .datum(seriesData)
+      .attr('fill', 'none')
+      .attr('stroke', color)
+      .attr('stroke-width', 3)
+      .attr('d', lineGenerator);
+  }
+
   private drawScatterplot(filters: FrequencyGraphFilter[]): void {
     this.svg.selectAll('*').remove();
-  
+
     let allDataPoints: Array<{ date: Date; count: number }> = [];
     const seriesByFilter = filters.map(filter => {
       const seriesData = filter.series
@@ -105,188 +188,68 @@ export class ScatterplotComponent implements OnInit, OnChanges, AfterViewInit, O
           return null;
         })
         .filter(d => d !== null) as Array<{ date: Date; count: number }>;
-  
+
       seriesData.sort((a, b) => a.date.getTime() - b.date.getTime());
       allDataPoints = allDataPoints.concat(seriesData);
       return { color: filter.color, seriesData };
     });
-  
+
     if (allDataPoints.length === 0) {
       const currentYear = new Date().getFullYear();
       const fallbackStartYearDate = new Date(currentYear - 4, 0, 1);
       const fallbackEndYearDate = new Date(currentYear, 0, 1);
       const gridTickInterval = 1;
       const maxCount = 1;
-  
-      const xScale = d3.scaleTime()
-        .domain([fallbackStartYearDate, fallbackEndYearDate])
-        .range([0, this.width]);
-  
-      const yScale = d3.scaleLinear()
-        .domain([0, maxCount])
-        .nice()
-        .range([this.height, 0]);
-  
+
+      const { xScale, yScale } = this.createScales(fallbackStartYearDate, fallbackEndYearDate, maxCount);
       const tickValues = d3.timeYear.range(
         fallbackStartYearDate,
         d3.timeYear.offset(fallbackEndYearDate, 1),
         gridTickInterval
       );
-  
-      const xGrid = this.svg.append("g")
-        .attr("class", "grid x-grid")
-        .attr("transform", `translate(0, ${this.height})`)
-        .call(
-          d3.axisBottom(xScale)
-            .tickValues(tickValues)
-            .tickSize(-this.height)
-            .tickFormat(() => "")
-        );
-      xGrid.select(".domain").remove();
-      xGrid.selectAll("line")
-        .attr("stroke", "gray")
-        .attr("stroke-opacity", 0.7);
-  
-      const yGrid = this.svg.append("g")
-        .attr("class", "grid y-grid")
-        .call(
-          d3.axisLeft(yScale)
-            .ticks(5)
-            .tickSize(-this.width)
-            .tickFormat(() => "")
-        );
-      yGrid.select(".domain").remove();
-      yGrid.selectAll("line")
-        .attr("stroke", "gray")
-        .attr("stroke-opacity", 0.7);
-  
-      const xAxis = d3.axisBottom(xScale)
-        .tickValues(tickValues)
-        .tickFormat(d3.timeFormat('%Y'));
-      const yAxis = d3.axisLeft(yScale)
-        .ticks(5);
-  
-      this.svg.append('g')
-        .attr('transform', `translate(0, ${this.height})`)
-        .call(xAxis);
-      this.svg.append('g')
-        .call(yAxis);
-  
-      this.svg.append('line')
-        .attr('class', 'right-border')
-        .attr('x1', this.width)
-        .attr('y1', 0)
-        .attr('x2', this.width)
-        .attr('y2', this.height)
-        .attr('stroke', 'black')
-        .attr('stroke-width', 1);
-  
+
+      this.drawGridAndAxes(xScale, yScale, gridTickInterval, tickValues);
+      this.drawRightBorder();
       return;
     }
-  
-    let [minDate, maxDate] = d3.extent(allDataPoints, d => d.date) as [Date, Date];
+
+    const [minDate, maxDate] = d3.extent(allDataPoints, d => d.date) as [Date, Date];
     const tenYearsAgo = new Date(new Date().getFullYear() - 10, 0, 1);
-    const startYearDate = (minDate > tenYearsAgo ? tenYearsAgo : minDate);
+    const startYearDate = minDate > tenYearsAgo ? tenYearsAgo : minDate;
     const endYearDate = new Date(maxDate.getFullYear(), 0, 1);
-  
+
     seriesByFilter.forEach(seriesObj => {
-      const originalData = seriesObj.seriesData;
       const dataMap = new Map<number, number>();
-      originalData.forEach(d => {
-        dataMap.set(d.date.getFullYear(), d.count);
-      });
+      seriesObj.seriesData.forEach(d => dataMap.set(d.date.getFullYear(), d.count));
+
       const completeData: Array<{ date: Date; count: number }> = [];
       for (let year = startYearDate.getFullYear(); year <= endYearDate.getFullYear(); year++) {
         completeData.push({ date: new Date(year, 0, 1), count: dataMap.get(year) ?? 0 });
       }
       seriesObj.seriesData = completeData;
     });
-  
+
     const dataSpanYears = endYearDate.getFullYear() - startYearDate.getFullYear();
-    let gridTickInterval: number = dataSpanYears >= 100 ? 10 : 5;
-  
+    const gridTickInterval = dataSpanYears >= 100 ? 10 : 5;
     const tickValues = d3.timeYear.range(
       startYearDate,
       d3.timeYear.offset(endYearDate, 1),
       gridTickInterval
     );
-  
+
     const completeAllDataPoints = seriesByFilter.flatMap(seriesObj => seriesObj.seriesData);
     const maxCount = d3.max(completeAllDataPoints, d => d.count) || 0;
-  
-    const xScale = d3.scaleTime()
-      .domain([startYearDate, endYearDate])
-      .range([0, this.width]);
-  
-    const yScale = d3.scaleLinear()
-      .domain([0, maxCount])
-      .nice()
-      .range([this.height, 0]);
-  
-    const xGrid = this.svg.append("g")
-      .attr("class", "grid x-grid")
-      .attr("transform", `translate(0, ${this.height})`)
-      .call(
-        d3.axisBottom(xScale)
-          .ticks(d3.timeYear.every(gridTickInterval))
-          .tickSize(-this.height)
-          .tickFormat(() => "")
-      );
-    xGrid.select(".domain").remove();
-    xGrid.selectAll("line")
-      .attr("stroke", "gray")
-      .attr("stroke-opacity", 0.7);
-  
-    const yGrid = this.svg.append("g")
-      .attr("class", "grid y-grid")
-      .call(
-        d3.axisLeft(yScale)
-          .ticks(5)
-          .tickSize(-this.width)
-          .tickFormat(() => "")
-      );
-    yGrid.select(".domain").remove();
-    yGrid.selectAll("line")
-      .attr("stroke", "gray")
-      .attr("stroke-opacity", 0.7);
-  
-    const xAxis = d3.axisBottom(xScale)
-      .ticks(d3.timeYear.every(gridTickInterval))
-      .tickFormat(d3.timeFormat('%Y'));
-    const yAxis = d3.axisLeft(yScale)
-      .ticks(5);
-  
-    this.svg.append('g')
-      .attr('transform', `translate(0, ${this.height})`)
-      .call(xAxis);
-  
-    this.svg.append('g')
-      .call(yAxis);
-  
+
+    const { xScale, yScale } = this.createScales(startYearDate, endYearDate, maxCount);
+
+    this.drawGridAndAxes(xScale, yScale, gridTickInterval);
+
     seriesByFilter.forEach(filterSeries => {
       if (filterSeries.seriesData.length > 0) {
-        const lineGenerator = d3.line<{ date: Date; count: number }>()
-          .x(d => xScale(d.date))
-          .y(d => yScale(d.count))
-          .curve(d3.curveLinear);
-  
-        this.svg.append('path')
-          .datum(filterSeries.seriesData)
-          .attr('fill', 'none')
-          .attr('stroke', filterSeries.color)
-          .attr('stroke-width', 3)
-          .attr('d', lineGenerator);
+        this.drawLine(filterSeries.seriesData, filterSeries.color, xScale, yScale);
       }
     });
-  
-    this.svg.append('line')
-      .attr('class', 'right-border')
-      .attr('x1', this.width)
-      .attr('y1', 0)
-      .attr('x2', this.width)
-      .attr('y2', this.height)
-      .attr('stroke', 'black')
-      .attr('stroke-width', 1);
+
+    this.drawRightBorder();
   }
-  
 }
